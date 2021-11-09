479233a946198293b37e05e863a1cd782e719cb5
Timyr_ychenik <timurka.styazhkin@mail.ru>
2021-11-09 20:59:54 +0300

    Паша, вопрос

--- YmnoshitRandomMatrix/YmnoshitRandomMatrix.java
+++ YmnoshitRandomMatrix/YmnoshitRandomMatrix.java
@@ -1,13 +1,25 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class YmnoshitRandomMatrix{
   public static void main(String[] args){
-		int width = (int) ((Math.random() * 10) + 1);
-    int height = (int) ((Math.random() * 10) + 1);
-    int [][] matrix_1 = new int[height][width];
-    int [][] matrix_2 = new int[height][width];
-    int [][] result = new int[height][width];
-    System.out.print("matriza : "+ "\n" +" width: "+ width + "\n" + "height: " + height + "\n");
+    Scanner scan = new Scanner(System.in);
+    System.out.println("matrix_1 width: ");
+    int width_1 = scan.nextInt();
+    System.out.println("matrix_1 height: ");
+    int height_1 = scan.nextInt();
+    System.out.println("matrix_2 width: ");
+    int width_2 = scan.nextInt();
+    System.out.println("matrix_2 height: ");
+    int height_2 = scan.nextInt();
+    int [][] matrix_1 = new int[height_1][width_1];
+    int [][] matrix_2 = new int[height_2][width_2];
+    int [][] result = new int[height_2][width_1];
+    if (height_1 != width_2) {
+      System.out.print("No, no, no");
+    }
+    // я сделал ввод ширины и высоты для каждой из матриц. Теперь задача состоит в том , чтобы наполнить эти матрицы числами
+    // для каждой матрицы следует созвать свой двойной алгоритм типа for(int i; i < ...; i++) {for(int j; j < ...; j++){заполнение матрицы   }} ???
     for (int i=0; i < height; i++){
       for (int j=0; j < width; j++){
        matrix_1[i][j] = (int) (Math.random() * 10);
